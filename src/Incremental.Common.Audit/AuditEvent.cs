2c0d2e860cdf73581908d7134533e7240e08af21
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-01-25 11:54:06 +0200

    feat: create AuditEvent

---
+++ src/Incremental.Common.Audit/AuditEvent.cs
@@ -0,0 +1,25 @@
+namespace Incremental.Common.Audit;
+
+public class AuditEvent
+{
+    public Guid Id { get; set; }
+    public DateTime StartDate { get; set; }
+    public DateTime EndDate { get; set; }
+    public int Duration { get; set; }
+    
+    public AuditEvent()
+    {
+        Id = Guid.NewGuid();
+    }
+
+    public void Start()
+    {
+        StartDate = DateTime.UtcNow;
+    }
+
+    public void End()
+    {
+        EndDate = DateTime.UtcNow;
+        Duration = (EndDate - StartDate).Duration().Milliseconds;
+    }
+}
