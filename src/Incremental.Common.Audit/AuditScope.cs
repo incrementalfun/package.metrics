4ccad756c9ae26e90ba9097b32ba90af4af27a64
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-01-25 12:08:32 +0200

    feat: update AuditScope to accept event in constructor

--- src/Incremental.Common.Audit/AuditScope.cs
+++ src/Incremental.Common.Audit/AuditScope.cs
@@ -5,16 +5,16 @@ namespace Incremental.Common.Audit;
 public class AuditScope : IAuditScope
 {
     private readonly IAuditStore _store;
-
-    private AuditEvent _event;
+    private readonly AuditEvent _event;
+    
     private bool _disposed;
     private bool _cancelled;
 
-    public AuditScope(IAuditStore store)
+    public AuditScope(IAuditStore store, AuditEvent @event)
     {
         _store = store;
+        _event = @event;
 
-        _event = new AuditEvent();
         _disposed = false;
         _cancelled = false;
     }
