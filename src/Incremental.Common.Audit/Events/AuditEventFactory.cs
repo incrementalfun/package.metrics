db017a4d57df44d1f95fafe8e6790ebd4d9e418f
Lucas Maximiliano Marino <hello@lucasmarino.me>
2022-01-25 14:21:10 +0200

    fix: add default to CancellationToken in CreateAuditEventAsync

--- src/Incremental.Common.Audit/Events/AuditEventFactory.cs
+++ src/Incremental.Common.Audit/Events/AuditEventFactory.cs
@@ -2,10 +2,10 @@ namespace Incremental.Common.Audit.Events;
 
 public class AuditEventFactory : IAuditEventFactory
 {
-    public async Task<TAuditEvent> CreateAuditEventAsync<TAuditEvent>(string eventName, CancellationToken cancellationToken) 
+    public async Task<TAuditEvent> CreateAuditEventAsync<TAuditEvent>(string eventName, CancellationToken cancellationToken = default) 
         where TAuditEvent : AuditEvent, new()
     {
-        return new TAuditEvent()
+        return new TAuditEvent
         {
             Name = eventName
         };
--- src/Incremental.Common.Audit/Events/IAuditEventFactory.cs
+++ src/Incremental.Common.Audit/Events/IAuditEventFactory.cs
@@ -2,5 +2,5 @@ namespace Incremental.Common.Audit.Events;
 
 public interface IAuditEventFactory
 {
-    Task<TAuditEvent> CreateAuditEventAsync<TAuditEvent>(string eventName, CancellationToken cancellationToken) where TAuditEvent : AuditEvent, new();
+    Task<TAuditEvent> CreateAuditEventAsync<TAuditEvent>(string eventName, CancellationToken cancellationToken = default) where TAuditEvent : AuditEvent, new();
 }
